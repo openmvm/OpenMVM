0b4e84c3793f2e072f6055fc65d02420c52f9229
OpenMVM <openmvm@gmail.com>
2022-11-01 21:14:58 +0700

    Update the framework.

--- system/Test/CIUnitTestCase.php
+++ system/Test/CIUnitTestCase.php
@@ -134,6 +134,7 @@ abstract class CIUnitTestCase extends TestCase
      * If not present, will use the defaultGroup.
      *
      * @var string
+     * @phpstan-var non-empty-string
      */
     protected $DBGroup = 'tests';
 
@@ -147,7 +148,7 @@ abstract class CIUnitTestCase extends TestCase
     /**
      * Migration Runner instance.
      *
-     * @var MigrationRunner|mixed
+     * @var MigrationRunner|null
      */
     protected $migrations;
 
