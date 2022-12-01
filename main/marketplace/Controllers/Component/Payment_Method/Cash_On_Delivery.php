c686ffc2dda48a289f859a0de7f62a48e06b588a
OpenMVM <openmvm@gmail.com>
2022-12-01 19:17:45 +0700

    Fix bugs

--- main/marketplace/Controllers/Component/Payment_Method/Cash_On_Delivery.php
+++ main/marketplace/Controllers/Component/Payment_Method/Cash_On_Delivery.php
@@ -22,6 +22,9 @@ class Cash_On_Delivery extends \App\Controllers\BaseController
         $this->weight = new \App\Libraries\Weight();
 
         // Models
+        $this->model_account_wallet = new \Main\Marketplace\Models\Account\Wallet_Model();
+        $this->model_account_wallet_temp = new \Main\Marketplace\Models\Account\Wallet_Temp_Model();
+        $this->model_component_component = new \Main\Marketplace\Models\Component\Component_Model();
         $this->model_customer_customer = new \Main\Marketplace\Models\Customer\Customer_Model();
         $this->model_customer_customer_address = new \Main\Marketplace\Models\Customer\Customer_Address_Model();
         $this->model_localisation_country = new \Main\Marketplace\Models\Localisation\Country_Model();
@@ -81,6 +84,53 @@ class Cash_On_Delivery extends \App\Controllers\BaseController
                 }
 
                 foreach ($sellers as $seller) {
+                    // Get order totals
+                    $order_totals = [];
+                    $order_total = 0;
+
+                    $order_total_data = [
+                        'totals' => &$order_totals,
+                        'taxes'  => &$taxes,
+                        'total'  => &$order_total
+                    ];
+
+                    $results = $this->model_component_component->getInstalledComponents('order_total');
+
+                    foreach ($results as $key => $value) {
+                        $sort_order[$key] = $this->setting->get('component_order_total_' . strtolower($value['value']). '_sort_order');
+                    }
+
+                    array_multisort($sort_order, SORT_ASC, $results);
+
+                    foreach ($results as $result) {
+                        if ($this->setting->get('component_order_total_' . strtolower($result['value']) . '_status')) {
+                            $namespace = '\Main\Marketplace\Models\Component\Order_Total\\' . $result['value'] . '_Model';
+
+                            $this->{'model_component_order_total_' . strtolower($result['value'])} = new $namespace;
+
+                            $this->{'model_component_order_total_' . strtolower($result['value'])}->getTotal($order_total_data, $seller['seller_id']);
+
+                        }
+                    }
+
+                    // Add wallet temp balance for seller
+                    $wallet_temp_description = [];
+
+                    foreach ($languages as $language) {
+                        $wallet_temp_description[$language['language_id']] = lang('Text.order_payment', [], $this->language->getCurrentCode());
+                    }
+
+                    $wallet_temp_input_data = [
+                        'order_id' => $this->session->get('checkout_order_id'),
+                        'seller_id' => $seller['seller_id'],
+                        'customer_id' => $seller['seller_id'],
+                        'amount' => $order_total,
+                        'description' => $wallet_temp_description,
+                        'comment' => '',
+                    ];
+
+                    $this->model_account_wallet_temp->addWalletTemp($seller['seller_id'], $wallet_temp_input_data);
+
                     // Add order status history
                     $order_id = $this->session->get('checkout_order_id');
 
