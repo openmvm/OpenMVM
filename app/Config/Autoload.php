b54e8330daf8bdf176c52250816a49ddad57b826
OpenMVM <openmvm@gmail.com>
2022-12-28 10:35:59 +0700

    Update the framework

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -34,11 +34,12 @@ class Autoload extends AutoloadConfig
      *```
      *   $psr4 = [
      *       'CodeIgniter' => SYSTEMPATH,
-     *       'App'	       => APPPATH
+     *       'App'         => APPPATH
      *   ];
      *```
      *
-     * @var array<string, string>
+     * @var array<string, array<int, string>|string>
+     * @phpstan-var array<string, string|list<string>>
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
@@ -87,8 +88,8 @@ class Autoload extends AutoloadConfig
      *
      * Prototype:
      * ```
-     *	  $files = [
-     *	 	   '/path/to/my/file.php',
+     *    $files = [
+     *        '/path/to/my/file.php',
      *    ];
      * ```
      *
